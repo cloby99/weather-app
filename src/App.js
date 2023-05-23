301f02990f9cdec35d6652b061949604b0810b1d
cloby7 <anuradhabsnk@gmail.com>
2023-05-23 16:29:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import UilReact from '@iconscout/react-unicons/icons/uil-react';
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+import getFormattedWeatherData from "./services/weatherService"
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+function App(){
+
+  const [query, setQuery] = useState({ q: "kandy" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : 'current location.';
+  
+      toast.info('Fetching weather for ' + message);
+  
+      await getFormattedWeatherData({ ...query, units }).then(data => {
+        toast.success(`Successfully fetched weather for ${data.name}, ${data.country}`)
+        setWeather(data);
+      });
+    };
+  
+    fetchWeather();
+  }, [query, units]);
+
+
+
+  const formatBackground = () => {
+      if (!weather) return "from-cyan-600 to-blue-700";
+      const threshold = units === "metric" ? 20 : 60;
+      if (weather.temp <= threshold) return "from-cyan-600 to-blue-700";
+
+      return "from-yellow-600 to-orange-600"
+
+  };
+
+  
+
+  
+
+
+  return(
+    <div className='bg-cyan'>
+      
+
+      <div className={`container mx-auto max-w-screen-md pb-5 pt-2 px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-400  ${formatBackground()}`}>
+            <TopButtons setQuery={setQuery}/>
+            <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
+
+          
+
+            {weather && (
+              <div>
+                <TimeAndLocation weather={weather} />
+                <TemperatureAndDetails weather={weather} />
+
+                <Forecast title={'hourly forecast'} items={weather.hourly} />
+                <Forecast title={'daily forecast'} items={weather.daily} />
+
+              </div>
+            )}
+
+          <ToastContainer autoClose={5000} theme='colored' newestOnTop={true} />
+
+          </div>
     </div>
-  );
+
+  )
 }
 
 export default App;
---
+++ src/services/weatherService.js
@@ -0,0 +1,90 @@
+import { DateTime } from 'luxon';
+
+const API_KEY = 'eb050777c753ea98d1ca65005a85d514';
+//const API_KEY = '8665a1fdffff456c833c5a6c79f1a328';
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url)
+    .then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  console.log("Current weather data:", data);
+
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed }
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat, lon, temp, feels_like, temp_min, temp_max, humidity,
+    name, dt, country, sunrise, sunset, details, icon, speed
+  };
+};
+
+const formatForecastWeather = (data) => {
+  let { timezone, daily, hourly } = data;
+
+  if (Array.isArray(daily)) {
+    daily = daily.slice(1, 6).map((d) => {
+      return {
+        title: formatToLocalTime(d.dt, timezone, 'ccc'),
+        temp: d.temp.day,
+        icon: d.weather[0].icon
+      };
+    });
+  } else {
+    daily = [];
+  }
+
+  if (Array.isArray(hourly)) {
+    hourly = hourly.slice(1, 6).map((d) => {
+      return {
+        title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
+        temp: d.temp,
+        icon: d.weather[0].icon
+      };
+    });
+  } else {
+    hourly = [];
+  }
+
+  return { timezone, daily, hourly };
+};
+
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData('weather', searchParams).then(formatCurrentWeather);
+
+  console.log("Formatted current weather:", formattedCurrentWeather);
+
+  const { lat, lon } = formattedCurrentWeather;
+
+  const formattedForecastWeather = await getWeatherData('onecall', {
+    lat,
+    lon,
+    exclude: 'current,minutely,alerts',
+    units: searchParams.units
+  }).then(formatForecastWeather);
+
+
+  return { ...formattedCurrentWeather, ...formattedForecastWeather };
+};
+
+const formatToLocalTime = (secs, zone, format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconUrlFromCode = (code) => `https://openweathermap.org/img/wn/${code}@2x.png`;
+
+export default getFormattedWeatherData;
+export { formatToLocalTime, iconUrlFromCode };
